e152ffa8e6e74476b4ced3e7c7575934e935f92c
david.giczi@gmail.com <david.giczi@gmail.com>
2025-01-07 12:19:36 +0100

    Regex

--- src/main/java/hu/david/giczi/mvmxpert/georegister/service/Note.java
+++ src/main/java/hu/david/giczi/mvmxpert/georegister/service/Note.java
@@ -29,39 +29,72 @@ public class Note {
 	}
 
 	public void setContent(String content) {
-		String[] contentParts = content.split("<br>");
 		StringBuilder sb = new StringBuilder();
 		String contentSytle = "style=color:";
-		
-		for (String contentPart : contentParts) {	
-		
-		if( contentPart.contains(contentSytle) ) {
-			sb.append(contentPart)
-			.append("<br>");
+		if( !content.contains("<") && !content.contains(">") ) {
+			this.content = content;
+			return;
 		}
-		else {	
+		else if( content.startsWith("<b") && content.contains(contentSytle) ) {
+			this.content = content;
+			return;
+		}
+		else if( content.startsWith("<b") && !content.contains(contentSytle) ) {
+			String[] parts = content.split("<b>|</b>");
+			this.content = "<b " + contentSytle + color + ">" + parts[1] + "</b>";
+			return;
+		}
+		String[] contentParts = content.split("</p>|</h[1-6]>");
+		String preTag;
+		String postTag;
+		
+		for (String part : contentParts) {
 			
-				if( contentPart.contains("<") && contentPart.contains(">") ) {
-					sb.append(contentPart.substring(0, contentPart.indexOf('>')))
-					.append(" ")
-					.append(contentSytle)
-					.append(color)
-					.append(contentPart.substring(contentPart.indexOf('>')) )
-					.append("<br>");
-				}
-				else if( !contentPart.contains("<") && !contentPart.contains(">") ) {
-					sb.append("<b")
-					.append(" ")
-					.append(contentSytle)
-					.append(color)
-					.append(">")
-					.append(contentPart)
-					.append("<b>")
-					.append("<br>");
-				}
+			if( part.startsWith("<br>") ) {
+				
+				if( !Character.isDigit(part.charAt(6)) && !part.contains(contentSytle) ){
+                    preTag = part.substring(4, 6) + " " + contentSytle + color + ">";
+                    String[] info = part.split("<p>|<h[1-6]>");
+                    postTag =  "</" + part.substring(5, 7);
+                    sb.append(preTag + info[1] + postTag);
+                }
+                else if( !Character.isDigit(part.charAt(6)) && part.contains(contentSytle) ){
+                	sb.append(part + "</" + part.charAt(5) + ">");
+                }
+                else if( Character.isDigit(part.charAt(6)) && part.contains(contentSytle) ){
+                	sb.append(part + "</" + part.substring(5,7) + ">");
+                }
+                else if( Character.isDigit(part.charAt(6)) && !part.contains(contentSytle) ){
+                    preTag = part.substring(4, 7) + " " + contentSytle + color + ">";
+                    String[] info = part.split("<p>|<h[1-6]>");
+                    postTag = "</" + part.substring(5, 7) + ">";
+                    sb.append(preTag + info[1] + postTag);
+                }
+				
 			}
-			this.content = sb.toString();
+			else {
+				
+				if( Character.isDigit(part.charAt(2)) && part.contains(contentSytle) ){
+					sb.append(part + "</" + part.substring(1,3) + ">");
+                }
+                else if( Character.isDigit(part.charAt(2)) && !part.contains(contentSytle) ){
+                    preTag = part.substring(0, 3) + " " + contentSytle + color + ">";
+                    String[] info = part.split("<p>|<h[1-6]>");
+                    postTag = "</" + part.substring(1, 3) + ">";
+                    sb.append(preTag + info[1] + postTag);
+                }
+                else if( !Character.isDigit(part.charAt(2)) && !part.contains(contentSytle) ){
+                    preTag = part.substring(0, 2) + " " + contentSytle + color + ">";
+                    String[] info = part.split("<p>|<h[1-6]>");
+                    postTag =  "</" + part.substring(1, 3);
+                    sb.append(preTag + info[1] + postTag);
+                }
+                else if( !Character.isDigit(part.charAt(2)) && part.contains(contentSytle) ){
+                	sb.append(part + "</" + part.charAt(1) + ">");
+                }			
+			}
 		}
+		this.content = sb.toString();
 	}
 
 	@Override
